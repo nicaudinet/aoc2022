1b136db7b13ecaebe263a9534c35e1fff56869f3
Nicolas Audinet <nic.audinet27@gmail.com>
2022-12-01 13:07:30 +0100

    Day 1

---
+++ day1.py
@@ -0,0 +1,19 @@
+with open("inputs/day1.txt") as file:
+    lines = file.readlines()
+
+lines = list(map(lambda s: s.strip(), lines))
+
+elves = []
+elf = []
+for line in lines:
+    if line == '':
+        elves.append(elf)
+        elf = []
+    else:
+        elf.append(int(line))
+
+calories = list(map(sum, elves))
+print(max(calories))
+
+calories.sort()
+print(sum(calories[-3:]))
